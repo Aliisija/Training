95043ff377df14720cafb02ff85ea90987d9bd0a
Ezalmanis <47554777+Ezalmanis@users.noreply.github.com>
2019-04-08 14:39:10 +0300

    task 1, spring initialized.

---
+++ src/main/java/com/accenture/op/task_domain/TaskDomainApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.op.task_domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskDomainApplication.class, args);
+	}
+
+}
