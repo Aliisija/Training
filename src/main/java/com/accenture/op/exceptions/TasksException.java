6f3ca6efad22ca55b86d01f2daeec069c5b2b54d
Aliisija <alisesvilpe@gmail.com>
2020-01-30 14:44:03 +0200

    Removed class

--- src/main/java/com/accenture/op/exceptions/TasksException.java
+++
@@ -1,8 +0,0 @@
-package com.accenture.op.exceptions;
-
-public class TasksException extends RuntimeException {
-
-    public TasksException(final String message) {
-        super(message);
-    }
-}
